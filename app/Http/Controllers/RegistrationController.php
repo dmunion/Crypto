c7b939ccf813c0a326c6a58f48fca4cfe7d968ee
Dmitriy <dmunion2018@gmail.com>
2021-11-14 16:36:26 +0200

    master

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Register User
+     *
+     * @param RegistrationRequest $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function register(RegistrationRequest $request)
+    {
+        User::create($request->getAttributes())->sendEmailVerificationNotification();
+
+        return $this->respondWithMessage('User successfully created');
+    }
+}
